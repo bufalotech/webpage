e33efa60758ce253f789cfd427c39cc2ae2ae8a1
Bruno Tenaglia <brunomartintenaglia@gmail.com>
2018-01-21 16:34:08 -0300

    first commit.

---
+++ README.md
@@ -0,0 +1,42 @@
+Installation
+============
+
+* Install dependencies
+
+```
+npm install
+```
+
+
+* And run it!
+
+```
+grunt
+```
+
+
+
+Development
+==========
+
+* Install the liveReload plugin for your browser
+* Type `grunt` to start watching
+* Add any new html templates into `app/src/templates/`, see changes happen live
+* `app/src/templates/common/base.html` contains the general template for the whole site
+* Add any css changes into `app/src/stylesheets/main.scss`, see changes happen live
+* You may want to place your variables in `app/src/stylesheets/_variables.scss`
+* Generate the site using `grunt dev`
+* ...
+* Profit!
+
+
+Production
+==========
+
+Run:
+
+```
+grunt prod
+```
+
+Collect your site code in `app/public/`
---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+var gulp = require('gulp');
+var nunjucksRender = require('gulp-nunjucks-render');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var data = require('gulp-data');
+var uglify = require('gulp-uglify');
+var runSequence = require('run-sequence');
+var cssnano = require('gulp-cssnano');
+var watch = require('gulp-watch');
+
+
+/* Nunjucks templates */
+gulp.task('nunjucks', function() {
+  // Gets .html and .nunjucks files in pages
+  return gulp.src('src/pages/**/*.+(html|nunjucks)')
+    // Adding data to Nunjucks
+    .pipe(data(function() {
+        return require('./src/data.json')
+    }))
+    // Renders template with nunjucks
+    .pipe(nunjucksRender({
+        path: ['src/templates']
+    }))
+    // output files in app folder
+    .pipe(gulp.dest('public'))
+});
+
+
+/* Javascript concat and minification */
+gulp.task('scripts', function() {
+  return gulp.src('src/js/*.js')
+    .pipe(concat('main.js')) // Concat all Js files in one
+    .pipe(uglify()) // Minifies the js
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('public/js'));
+});
+
+/* Javascript copy thirst libs */
+gulp.task('scripts-libs', function() {
+  return gulp.src('src/js/libs/*.js')
+      .pipe(gulp.dest('public/js'));;
+});
+
+/* Css concat and minification */
+gulp.task('styles', function() {
+  return gulp.src('src/css/*.css')
+    .pipe(concat('main.css')) // Concat all css files in one
+    .pipe(cssnano()) // Minifies the js
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('public/css'));
+});
+
+/* Css copy thirst libs */
+gulp.task('styles-libs', function() {
+  return gulp.src('src/css/libs/*.css')
+      .pipe(gulp.dest('public/css'));;
+});
+
+/* Copy images */
+gulp.task('images', function() {
+  return gulp.src('src/img/**/*.{gif,jpg,png,svg}')
+      .pipe(gulp.dest('public/img'));
+});
+
+/* Copy fonts */
+gulp.task('fonts', function() {
+  return gulp.src('src/fonts/**/*.*')
+      .pipe(gulp.dest('public/fonts'));
+});
+
+/* Watch */
+gulp.task('watch', function() {
+
+  gulp.watch('src/js/*.js', ['scripts']);
+
+  gulp.watch('src/js/libs/*.js', ['scripts-libs']);
+
+  gulp.watch('src/css/*.css', ['styles']);
+
+  gulp.watch('src/css/libs/*.css', ['styles-libs']);
+
+  gulp.watch('src/fonts/**/*.*', ['fonts']);
+
+  gulp.watch('src/img/**/*.{gif,jpg,png,svg}', ['images']);
+
+  gulp.watch(['src/templates/**/*.html', 'src/*.json'], ['nunjucks']);
+
+});
+
+
+gulp.task('default', function() {
+    runSequence('nunjucks', 'scripts', 'styles', 'styles-libs', 'images', 'fonts', 'scripts-libs');
+})
+
